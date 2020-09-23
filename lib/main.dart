09efcee5b2bf4d33f7e2a66b3806d479a3fbb9cd
John Kilima <jkbundala@gmail.com>
2020-09-23 20:01:10 +0300

    TextField

--- lib/main.dart
+++ lib/main.dart
@@ -28,6 +28,28 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
+  String displayText = '';
+
+  final textController = TextEditingController();
+
+  changeText() {
+    setState(() {
+      this.displayText = 'Activated airplane mode';
+    });
+  }
+
+  pressButton() {
+    setState(() {
+      displayText = textController.text.toString();
+    });
+  }
+
+  @override
+  void dispose() {
+    textController.dispose();
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -38,26 +60,76 @@ class _MyHomePageState extends State<MyHomePage> {
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
+            // TextFields
+            TextField(
+              decoration: InputDecoration(hintText: 'Enter text here'),
+              textAlign: TextAlign.center,
+              textCapitalization: TextCapitalization.characters,
+              textInputAction: TextInputAction.go,
+              keyboardType: TextInputType.number,
+              controller: textController,
+              // onSubmitted: (text) {
+              //   setState(() {
+              //     displayText = text;
+              //   });
+              // },
+              // onChanged: (text) {
+              //   setState(() {
+              //     displayText = text;
+              //   });
+              // },
+            ),
+            MaterialButton(
+              onPressed: () => pressButton(),
+              child: Text('Press Me'),
+            ),
+            Text(displayText)
+
+            // Button
+            // MaterialButton(
+            //   onPressed: () => changeText(),
+            //   child: Row(
+            //     mainAxisSize: MainAxisSize.min,
+            //     children: [
+            //       Icon(
+            //         Icons.airplanemode_active,
+            //         color: Colors.white,
+            //       ),
+            //       Text('Press me'),
+            //     ],
+            //   ),
+            //   color: Colors.black,
+            //   textColor: Colors.white,
+            //   highlightColor: Colors.blue, //When long pressed
+            //   splashColor: Colors.red,
+            //   elevation: 10,
+            // ),
+            // Padding(
+            //   padding: const EdgeInsets.all(16.0),
+            //   child: Text(this.displayText),
+            // ),
+
             // Text Widget
-            Text(
-              'Some Text here',
-              style: TextStyle(
-                color: Colors.blue,
-                fontSize: 50,
-                fontStyle: FontStyle.italic,
-                fontWeight: FontWeight.bold,
-                letterSpacing: 2,
-                shadows: [
-                  Shadow(
-                    color: Colors.grey,
-                    offset: Offset(-5, -5),
-                  ),
-                ],
-                decoration: TextDecoration.underline,
-                decorationColor: Colors.red,
-                decorationStyle: TextDecorationStyle.wavy
-              ),
-            )
+            // Text(
+            //   'Some Text here',
+            //   style: TextStyle(
+            //     color: Colors.blue,
+            //     fontSize: 50,
+            //     fontStyle: FontStyle.italic,
+            //     fontWeight: FontWeight.bold,
+            //     letterSpacing: 2,
+            //     shadows: [
+            //       Shadow(
+            //         color: Colors.grey,
+            //         offset: Offset(-5, -5),
+            //       ),
+            //     ],
+            //     decoration: TextDecoration.underline,
+            //     decorationColor: Colors.red,
+            //     decorationStyle: TextDecorationStyle.wavy
+            //   ),
+            // )
+
             // Column and Row Widgets
             // Row(
             //   mainAxisAlignment: MainAxisAlignment.center,
